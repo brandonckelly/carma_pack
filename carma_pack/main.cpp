19f7ff75c9c748628b81509abf946d1e77cbe7dc
Brandon C. Kelly <bckelly80@gmail.com>
2013-04-26 17:28:45 -0400

    Initial Commit

---
+++ carma_pack/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  carma_pack
+//
+//  Created by Brandon Kelly on 4/26/13.
+//  Copyright (c) 2013 Brandon Kelly. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
